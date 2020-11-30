f5be7a49b3cc68943ce4e43023c5d8302851a2d5
YoavFlato <yoav.flato@mail.huji.ac.il>
2020-11-30 15:02:34 +0200

    -- added approx_rocket_ode

--- rocket_pather.py
+++ rocket_pather.py
@@ -1,7 +1,8 @@
 from rocket_data import *
+import math
 
 
-def approx_rocket_ode(dt, t, x0, y0, vx0, vy0, drag_coef):
+def approx_rocket_ode(dt, t, x0, y0, vx0, vy0, drag_coef, m, g):
     """
 
     :param dt: time of sim step
@@ -10,10 +11,16 @@ def approx_rocket_ode(dt, t, x0, y0, vx0, vy0, drag_coef):
     :param y0: start y
     :param v0: start v (size of velocity vector)
     :param theta0: start angle (argument of velocity vector)
-    :param drag_coef: the drag
+    :param drag_coef: the drag - CAp
     :return a list (t1, x1, y1, vx1, vy1) describing the state of the rocket after dt time
     """
-    return 0, 0, 0, 0, 0
+    v_size = math.sqrt(math.pow(vx0, 2) + math.pow(vx0, 2))
+    t1 = t + dt
+    x1 = x0 + vx0 * dt
+    y1 = y0 + vy0 * dt
+    vx1 = vx0 - drag_coef * v_size * vx0 * dt
+    vy1 = vy0 - (drag_coef * v_size * vy0 + m * g) * dt
+    return t1, x1, y1, vx1, vy1
 
 
 def rocket_in_vaccum(dt, x0, y0, v0, theta0):
@@ -59,4 +66,3 @@ def find_theta(dt, rocket_data):
 
 def find_minimal_distance(rocket_data_first, rocket_data_second):
     pass
-
