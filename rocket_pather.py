e9b91177fd77d52a0cc6fdcc86ae12686461f96e
Alexey <theemailreader300@gmail.com>
2020-11-30 14:49:58 +0200

    ads

--- rocket_pather.py
+++ rocket_pather.py
@@ -1,7 +1,7 @@
 from rocket_data import *
 
 
-def approx_rocket_ode(dt, t, x0, y0, v0, theta0, drag_coef):
+def approx_rocket_ode(dt, t, x0, y0, vx0, vy0, drag_coef):
     """
 
     :param dt: time of sim step
@@ -11,7 +11,7 @@ def approx_rocket_ode(dt, t, x0, y0, v0, theta0, drag_coef):
     :param v0: start v (size of velocity vector)
     :param theta0: start angle (argument of velocity vector)
     :param drag_coef: the drag
-    :return a list (t1, x1, y1, v1, theta1) describing the state of the rocket after dt time
+    :return a list (t1, x1, y1, vx1, vy1) describing the state of the rocket after dt time
     """
     return 0, 0, 0, 0, 0
 
@@ -26,10 +26,10 @@ def rocket_in_vaccum(dt, x0, y0, v0, theta0):
     :param theta0: start angle (argument of velocity vector)
     :return: generator returing the next point each time
     """
-    return rocket_in_air(dt, RocketData(x0, y0 , b0, theta0, 0))
+    return rocket_in_air(dt, RocketData(x0, y0, v0, theta0, 0))
 
 
-def rocket_in_air(dt, rocket_data):
+def rocket_in_air(dt, rocket_data: RocketData):
     """
 
     :param dt: time of sim step
@@ -40,7 +40,13 @@ def rocket_in_air(dt, rocket_data):
     :param drag_coef: the drag (C * rho * A)
     :return: generator returing the next point each time
     """
-    pass
+    vel_vect = roccket_data.get_vel_vector()
+    t = 0
+    data = (t, rocket_data.x, rocket_data.y, vel_vect[0], vel_vect[1])
+
+    yield data[1], data[2]
+    while True:
+        next = ()
 
 
 def get_hit_loc(dt, rocket_data):
